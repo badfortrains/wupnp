4a9ed12b644e57c14ab89c2abe1a2d663bbf964a
Sean Purcell <sean.purcell@tapjoy.com>
2013-03-19 21:41:49 -0700

    grunt for concatinating / minifying

--- routes/wu.js
+++ routes/wu.js
@@ -6,15 +6,17 @@ exports.index = function(req, res){
   Playlists.prototype.findList({},{name:1},function(err,docs){
     var lists = "",
         renderers = "var bootstrapRenderers =" + JSON.stringify(Renderes.all()),
-        servers = "var bootstrapServers =" + JSON.stringify(Servers.all());
+        servers = "var bootstrapServers =" + JSON.stringify(Servers.all()),
+        template = (process.env.NODE_ENV === 'production') ? 'Wu-prod' : 'Wu'; 
+
+    console.log(process.env.NODE_ENV)
     if(!err && docs){
       lists = "var bootstrapPlaylists = " + JSON.stringify(docs);
     }else{
       lists = "var bootstrapPlaylists = [];"
     }
 
-    console.log(servers)
-    res.render('Wu', { 
+    res.render(template, { 
       title: 'Wu',
       playlists: lists,
       servers: servers,
