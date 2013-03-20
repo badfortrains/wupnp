60205c2533e5a1bf0ccf18dcec01e7179ec9e144
Sean Purcell <sean.purcell@tapjoy.com>
2013-03-19 22:44:54 -0700

    Bug fix: error for destroying models

--- controllers/playlists_tracks_controller.js
+++ controllers/playlists_tracks_controller.js
@@ -29,7 +29,7 @@ module.exports = {
       if(err){
         res.send(500,"failed to remove track");
       }else{
-        res.send(200);
+        res.send(200,{deleted:true});
       }
     })
   }
